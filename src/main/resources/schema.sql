9e609f5f0852c8c17c89317acc0f2ab27e4515ac
ElenaKlimkina <klimkina201485@gmail.com>
2024-05-09 11:48:00 +0700

    fix: убрать валидацию добавления лайка

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -80,8 +80,7 @@ CREATE TABLE IF NOT EXISTS review_rate (
 
 CREATE TABLE IF NOT EXISTS likes (
     film_id INTEGER REFERENCES film(film_id) ON DELETE CASCADE,
-    user_id INTEGER REFERENCES users(user_id) ON DELETE CASCADE,
-    PRIMARY KEY (film_id, user_id)
+    user_id INTEGER REFERENCES users(user_id) ON DELETE CASCADE
 );
 
 
